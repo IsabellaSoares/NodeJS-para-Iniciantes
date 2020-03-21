02bdfce7038761b2111562646ef991e442ad552a
Isabella <isabella.soares.28@gmail.com>
2020-03-21 09:42:01 -0300

    array reduce

---
+++ 04-lists/for-forin/4 - reduce.js
@@ -0,0 +1,23 @@
+const { obterPessoas } = require('./service');
+
+async function main () {
+    try {
+        const { results } = await obterPessoas('a');
+        const pesos = results.map(pessoa => parseInt(pessoa.height));
+
+        const total = pesos.reduce((anterior, proximo) => anterior + proximo, 0);
+        console.log('total', total);
+
+        const minhaLista = [
+            ['Isa', 'Soares'],
+            ['NodeJS', 'ReactJS']
+        ]
+
+        const listaTotal = minhaLista.reduce((anterior, proximo) => anterior.concat(proximo), []).join(', ');
+        console.log('listaTotal', listaTotal);
+    } catch (error) {
+        console.error('error', error);
+    }
+}
+
+main();
