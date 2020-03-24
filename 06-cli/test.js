4babb09d8ddce801eed22493df9ffcab3f9045c3
Isabella <isabella.soares.28@gmail.com>
2020-03-23 21:29:06 -0300

    writing files

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -4,13 +4,20 @@ const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'Speed', id: 1 };
 const database = require('./database');
 
 describe('Suíte de manipulação de Herois', () => {
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    });
+
     it('deve pesquisa um heroi, usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR;
         const [resultado] = await database.listar(expected.id);
         deepEqual(resultado, expected);
     });
-    // it('deve cadastrar um heroi, usando arquivos', async () => {
-    //     const expected = DEFAULT_ITEM_CADASTRAR;
-    //     ok(null, expected);
-    // });
+
+    it('deve cadastrar um heroi, usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR;
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+        const [atual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
+        deepEqual(atual, expected);
+    });
 })
