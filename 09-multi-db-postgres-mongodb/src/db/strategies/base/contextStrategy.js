d971735ef7a99d256894e8a91f7ed1277cfa4b07
Isabella <isabella.soares.28@gmail.com>
2020-04-01 22:50:03 -0300

    finish MongoDB

--- 09-multi-db-postgres-mongodb/src/db/strategies/base/contextStrategy.js
+++ 09-multi-db-postgres-mongodb/src/db/strategies/base/contextStrategy.js
@@ -1,33 +1,26 @@
-const ICrud = require('../interfaces/interfaceCrud');
-
-class ContextStrategy extends ICrud {
-  constructor (strategy) {
+const IDb = require('./interfaceDb');
+class ContextStrategy extends IDb {
+  constructor(database) {
     super();
-    this._database = strategy;
+    this._database = database;
   }
-
-  create (item) {
-    return this._database.create(item)
-  }
-
-  read (query) {
-    return this._database.read(query);
-  }
-
-  update (id, item) {
-    return this._database.update(id, item);
-  }
-
-  delete (id) {
-    return this._database.delete(id);
-  }
-
-  isConnected () {
+  isConnected() {
     return this._database.isConnected();
   }
-
-  connect () {
-    return this._database.connect();
+  connect() {
+    return this._database.connect()
+  }
+  create(item) {
+    return this._database.create(item);
+  }
+  read(item) {
+    return this._database.read(item);
+  }
+  update(id, item) {
+    return this._database.update(id, item);
+  }
+  delete(id) {
+    return this._database.delete(id);
   }
 }
 
--- 09-multi-db-postgres-mongodb/src/db/strategies/mongodb.js
+++ 09-multi-db-postgres-mongodb/src/db/strategies/mongodb.js
@@ -1,12 +1,70 @@
 const ICrud = require('./interfaces/interfaceCrud');
 
+const Mongoose = require('mongoose');
+
+const STATUS = {
+  0: 'Desconectado',
+  1: 'Conectado',
+  2: 'Conectando',
+  3: 'Desconectando'
+}
+
 class MongoDB extends ICrud {
   constructor () {
     super();
+    this._driver = null;
+    this._herois = null;
+  }
+  
+  async isConnected () {
+    const state = Mongoose.connection.readyState;
+
+    if (state === 'Conectado') return true;
+    if (state !== 'Conectando') return false;
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return STATUS[Mongoose.connection.readyState];
+  }
+  
+  defineModel () {
+    this._herois = new Mongoose.Schema({
+      nome: {
+        type: String,
+        required: true
+      },
+      poder: {
+        type: String,
+        required: true
+      },
+      insertedAt: {
+        type: Date,
+        default: new Date()
+      }
+    });
+
+    const model = Mongoose.model('herois', heroiSchema);
+  }
+  
+  connect () {
+    Mongoose.connect('mongodb://meu_usuario:minha_senha@localhost:27017/herois',
+      { useNewUrlParser: true }, function (error) {
+        if (!error) return;
+        console.log('Falha na conexão!', error);
+      }
+    );
+    
+    const connection = Mongoose.connection;
+
+    connection.once('open', () => {
+      console.log('Database rodando!');
+    });
   }
 
   create (item) {
-    console.log('O item foi salvo em MongoDB.');
+    const resultCadastrar = await model.create({
+      nome: 'Batman',
+      poder: 'Dinheiro'
+    });
   }
 }
 
